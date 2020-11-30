68ce947149d3827f18d17cea19563319cce324fd
Josh Braga <joshbraga@outlook.com>
2020-11-30 17:23:43 -0500

    Built initial validation and unit tested

--- WDD-A6-WebServer/HTTPHandler.cs
+++ WDD-A6-WebServer/HTTPHandler.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+ *  FILE            :   HttpHandler.cs
+ *  PROJECT         :   WDD-A6-WebServer
+ *  PROGRAMMER      :   Josh Braga and Balazs Karner
+ *  FIRST VERSION   :   2020-11-30
+ */
+
+/*
+ *  NAME            :   HttpHandler
+ *  PURPOSE         :   Processing HTTP requests and building a response to 
+ *                      be sent back to the client.
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +20,51 @@ using System.Threading.Tasks;
 
 namespace WDD_A6_WebServer
 {
-    class HTTPHandler
+    public class HttpHandler
     {
+        public string Method { get; private set; }
+        public string RequestURI { get; private set; }
+        public string HTTPVersion { get; private set; }
+        public int StatusCode { get; private set; }
 
 
-        string ParseRequest()
+
+
+        // METHOD               :   ValidateAndSetRequest
+        // DESCRIPTION          :   This method does the initial validation of the 
+        //                          HTTP request to see if its structure is valid.
+        //
+        // PARAMETERS           :
+        //  string request      :   contains the request line from the client
+        //  string serverInfo   :   contains the ip address and port of the server
+        //                          to validate against the URI portion of request
+        //
+        // RETURNS              :
+        //  Boolean             :   returns true if request is valid, false otherwise
+        //
+        public Boolean ValidateAndSetRequest(string request, string serverInfo)
         {
+            Boolean isValid = false;
 
-            return "test";
+            string[] split = request.Split(' ');
+
+            if (split[0] == "GET")
+            {
+                if (split[1].StartsWith(@"http://" + serverInfo + @"/"))
+                {
+                    if (split[2] == @"HTTP/1.1")
+                    {
+                        Method = split[0];
+                        RequestURI = split[1];
+                        HTTPVersion = split[2];
+                        isValid = true;
+                    }
+                }
+            }
+
+            return isValid;
         }
 
+        
     }
 }
