a1432356e000f25ab90745632f081f1bfe45977d
Josh Braga <joshbraga@outlook.com>
2020-11-30 05:13:30 -0500

    Add project files.

---
+++ WDD-A6-WebServer/HTTPHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDD_A6_WebServer
+{
+    class HTTPHandler
+    {
+    }
+}
---
+++ WDD-A6-WebServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDD_A6_WebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerListener server = new ServerListener(@"C:\", "127.0.0.1", 13000);
+            server.StartServer();
+        }
+
+
+    }
+}
